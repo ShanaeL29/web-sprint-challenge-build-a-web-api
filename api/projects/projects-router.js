a57bc8da1061aa3de360bde1ec58b89ba49bb904
Shanae <sleslie18@gmail.com>
2022-01-14 15:50:36 -0500

    DELETE and getProjectActions

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -31,6 +31,25 @@ router.post("/", checkProjectPayload, (req, res, next) => {
     .catch(next);
 });
 
+router.delete("/:id", checkProjectId, (req, res, next) => {
+  Project.remove(req.params.id).then().res.status(200).catch(next);
+});
+
+router.get("/:id/actions", checkProjectId, (req, res, next) => {
+  Project.getProjectActions(req.params.id).then((actions) => {
+    if (actions) {
+      res.status(200).json(actions);
+    } else {
+      res.json([]);
+
+      //   next({
+      //     status: 404,
+      //     message: "Not found",
+      //   });
+    }
+  });
+});
+
 router.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     custom: "something went wrong in the projects router",
